19c884ed4141c8d62fa710ffb104a46d919a4f2d
Joseph Cooper <cooper.joseph@gmail.com>
2021-12-03 23:01:00 +0000

    Scaffolding for day 2

---
+++ day02/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn part1(input: &str) -> String {
+    unimplemented!()
+}
+
+pub fn part2(input: &str) -> String {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    const TEST_INPUT: &str = indoc! {"
+
+		"
+    };
+
+    #[test]
+    fn part1_test_input() {
+        assert_eq!("", part1(TEST_INPUT))
+    }
+
+    #[test]
+    fn part2_test_input() {
+        assert_eq!("", part2(TEST_INPUT))
+    }
+}
