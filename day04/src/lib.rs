88271fbc247c49dfad2e02f8ff330a759663d671
Joseph Cooper <cooper.joseph@gmail.com>
2021-12-14 19:48:39 +0000

    Completed day4/part 1

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -1,7 +1,89 @@
 use color_eyre::eyre::{eyre, Result};
 
+type Cell = (u32, bool);
+type Board = [[Cell; 5]; 5];
+
 pub fn part1(input: &str) -> Result<u32> {
-    Err(eyre!("Not implemented"))
+    let mut lines = input.lines();
+    let moves = parse_moves(lines.next().ok_or_else(|| eyre!("No lines of input!"))?)?;
+    let mut boards = parse_boards(&lines.collect::<Vec<_>>().chunks(6).collect::<Vec<_>>())?;
+    let mut winner: Option<(u32, Board)> = None;
+
+    'moves: for m in moves.iter() {
+        for board in boards.iter_mut() {
+            for row in board.iter_mut() {
+                for (entry, selected) in row.into_iter() {
+                    if !*selected && entry == m {
+                        *selected = true;
+                    }
+                }
+            }
+            if is_board_a_winner(&board) {
+                winner = Some((*m, *board));
+                break 'moves;
+            }
+        }
+    }
+    winner
+        .map(|(m, board)| {
+            let sum_of_unselected: u32 = board
+                .iter()
+                .flat_map(|row| row.iter().filter(|(_, selected)| !selected))
+                .map(|(n, _)| n)
+                .sum();
+            m * sum_of_unselected
+        })
+        .ok_or_else(|| eyre!("No winning board found"))
+}
+
+fn is_board_a_winner(board: &Board) -> bool {
+    for &row in board {
+        if row.iter().all(|&(_, selected)| selected) {
+            return true;
+        }
+    }
+    for i in 0..5 {
+        if board
+            .iter()
+            .map(|&row| row[i])
+            .all(|(_, selected)| selected)
+        {
+            return true;
+        }
+    }
+    false
+}
+
+fn parse_moves(line: &str) -> Result<Vec<u32>> {
+    let moves = line
+        .split(',')
+        .map(|s| s.parse::<u32>())
+        .collect::<Result<Vec<u32>, _>>()?;
+    Ok(moves)
+}
+
+fn parse_boards(chunks: &[&[&str]]) -> Result<Vec<Board>> {
+    let mut boards: Vec<Board> = vec![];
+    for &chunk in chunks {
+        let mut board: Vec<[Cell; 5]> = vec![];
+        for &line in chunk.iter().skip(1) {
+            let row = line
+                .split_whitespace()
+                .map(|entry| -> Result<_, _> { Ok((entry.parse::<u32>()?, false)) })
+                .collect::<Result<Vec<Cell>>>()?;
+            let row: [Cell; 5] = row.try_into().unwrap();
+
+            board.push(row);
+        }
+        boards.push(board.try_into().unwrap());
+    }
+    // chunks
+    //     .map(|&lines| lines.iter().skip(1).map(|line|
+    //         line.split_whitespace().map(|entry|(entry.parse::<u32>(), false))).collect::<Vec<_>>()
+    //     .try_into();).collect::<Vec<_>>()
+    //     .try_into();
+    //     ).collect();
+    Ok(boards)
 }
 
 pub fn part2(input: &str) -> Result<String> {
