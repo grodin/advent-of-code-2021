f8252fc1bec852c2f696d12df196c36d784f73a0
Joseph Cooper <cooper.joseph@gmail.com>
2021-12-20 22:32:56 +0000

    Solved day 7/part 1

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,18 +1,56 @@
-use color_eyre::eyre::{eyre, Result};
+use color_eyre::eyre::{eyre, Context, Result};
 
-pub fn part1(input: &str) -> Result<String> {
+pub fn part1(input: &str) -> Result<u32> {
+    let count_by_position = parse_input(input)?;
+    let mut fuel_champion = u32::MAX;
+    for i in 0..count_by_position.len() {
+        let fuel_candidate: u32 = count_by_position
+            .iter()
+            .enumerate()
+            .map(|(pos, count)| calculate_fuel_cost(count, &pos, &i))
+            .collect::<Result<Vec<_>, _>>()?
+            .into_iter()
+            .sum();
+        if fuel_candidate < fuel_champion {
+            fuel_champion = fuel_candidate
+        }
+    }
+    Ok(fuel_champion)
+}
+
+fn calculate_fuel_cost(
+    count: &u16,
+    original_position: &usize,
+    destination_position: &usize,
+) -> Result<u32> {
+    let count = *count as i128;
+    let orig_pos = *original_position as i128;
+    let dest_pos = *destination_position as i128;
+    Ok((count * (orig_pos - dest_pos).abs()).try_into()?)
+}
+pub fn part2(input: &str) -> Result<u64> {
     Err(eyre!("Not implemented"))
 }
 
-pub fn part2(input: &str) -> Result<String> {
-    Err(eyre!("Not implemented"))
-}
+type CountByPosition = Box<[u16]>;
 
-fn parse_input(input: &str) -> Result<Vec<_>> {
-    input
-        .lines()
-        .map(|s| s.trim())
+fn parse_input(input: &str) -> Result<CountByPosition> {
+    let initial_positions = input
+        .split(',')
+        .map(|s| s.trim().parse::<usize>())
         .collect::<Result<Vec<_>, _>>()
+        .wrap_err_with(|| "Error while parsing")?;
+    let max_position = initial_positions
+        .iter()
+        .max()
+        .ok_or_else(|| eyre!("Error finding max initial position"))?
+        .clone() as usize;
+    let mut count_by_position = vec![0_u16; max_position + 1];
+    for pos in initial_positions {
+        count_by_position[pos] += 1;
+    }
+
+    Ok(count_by_position.into_boxed_slice())
 }
 
 #[cfg(test)]
@@ -24,13 +62,13 @@ mod tests {
 
     #[test]
     fn part1_test_input() -> Result<()> {
-        assert_eq!("", part1(TEST_INPUT)?);
+        assert_eq!(37, part1(TEST_INPUT)?);
         Ok(())
     }
 
     #[test]
     fn part2_test_input() -> Result<()> {
-        assert_eq!("", part2(TEST_INPUT)?);
+        assert_eq!(0, part2(TEST_INPUT)?);
         Ok(())
     }
 }
