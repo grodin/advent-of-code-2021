bf93c4ce291a580620a1f894b8cfa63aed8e7712
Joseph Cooper <cooper.joseph@gmail.com>
2021-12-20 20:42:36 +0000

    Completed day 6

--- day06/src/lanternfish.rs
+++ day06/src/lanternfish.rs
@@ -1,40 +1,23 @@
 use std::str::FromStr;
 
-use color_eyre::eyre::{eyre, Report};
+use color_eyre::eyre::{eyre, Context, Report};
 
 #[derive(Debug)]
 pub struct LanternFish {
-    age: u8,
+    pub age: usize,
 }
 
 impl LanternFish {
-    const BIRTH_AGE: u8 = 8;
-    const REBIRTH_AGE: u8 = 6;
-    fn age(&self) -> u8 {
-        self.age
-    }
-
-    pub fn step_day(&mut self) -> Option<LanternFish> {
-        let (new_age, new_fish) = match self.age {
-            0 => (
-                Self::REBIRTH_AGE,
-                Some(LanternFish {
-                    age: Self::BIRTH_AGE,
-                }),
-            ),
-            1..=Self::BIRTH_AGE => (self.age() - 1, None),
-            _ => panic!("Lantern fish with age out of bounds: {}", self.age),
-        };
-        self.age = new_age;
-        new_fish
-    }
+    const BIRTH_AGE: usize = 8;
 }
 
 impl FromStr for LanternFish {
     type Err = Report;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let age = s.parse::<u8>()?;
+        let age = s
+            .parse::<usize>()
+            .wrap_err_with(|| format!("Error parsing {} as u8", s))?;
         (age <= Self::BIRTH_AGE)
             .then(|| LanternFish { age })
             .ok_or_else(|| {
--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,28 +1,44 @@
-use color_eyre::eyre::{eyre, Result};
+use color_eyre::eyre::Result;
 use lanternfish::LanternFish;
 
 mod lanternfish;
 
-pub fn part1(input: &str) -> Result<usize> {
-    let mut fish = parse_list_of_fish(input)?;
-    let mut new_fish: Vec<LanternFish> = vec![];
-    for _ in 0..80 {
-        for f in &mut fish {
-            if let Some(new_f) = f.step_day() {
-                new_fish.push(new_f);
-            }
-        }
-        fish.append(&mut new_fish);
-    }
-    Ok(fish.len())
+pub fn part1(input: &str) -> Result<u64> {
+    solution(input, 80)
 }
 
-pub fn part2(input: &str) -> Result<String> {
-    Err(eyre!("Not implemented"))
+pub fn part2(input: &str) -> Result<u64> {
+    solution(input, 256)
+}
+
+fn solution(input: &str, num_days_to_simulate: usize) -> Result<u64> {
+    let fish = parse_list_of_fish(input)?;
+    let mut fish_count_by_age = [0; 9];
+    for f in &fish {
+        fish_count_by_age[f.age] += 1;
+    }
+
+    for _ in 0..num_days_to_simulate {
+        let num_new_fish = fish_count_by_age[0];
+        // Fish that are resetting will be added back in after the loop
+        fish_count_by_age[0] = 0;
+
+        for state in 1..9 {
+            let fish_to_move = fish_count_by_age[state];
+            fish_count_by_age[state - 1] += fish_to_move;
+            fish_count_by_age[state] -= fish_to_move;
+        }
+        //Reset fish who are spawning new fish to age 6
+        fish_count_by_age[6] += num_new_fish;
+        //Spawn new fish
+        fish_count_by_age[8] = num_new_fish;
+    }
+    Ok(fish_count_by_age.iter().sum())
 }
 
 fn parse_list_of_fish(input: &str) -> Result<Vec<LanternFish>> {
     input
+        .trim()
         .split(',')
         .map(|s| s.parse::<LanternFish>())
         .collect::<Result<Vec<_>, _>>()
@@ -43,7 +59,7 @@ mod tests {
 
     #[test]
     fn part2_test_input() -> Result<()> {
-        assert_eq!("", part2(TEST_INPUT)?);
+        assert_eq!(26_984_457_539, part2(TEST_INPUT)?);
         Ok(())
     }
 }
